85f68847ff5e51ce442b0a908d2a930f22a1e287
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-10-22 11:26:37 +0200

    init new project

---
+++ src/main/java/com/bookshop/bookmanagement/BookshopBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopBookManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopBookManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookshop/bookmanagement/BookshopBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopBookManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
